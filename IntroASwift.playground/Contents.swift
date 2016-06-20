ca51635578b10dde78cdc1afa4783a33896a5059
icapa <ivan.olea@gmail.com>
2016-06-20 22:00:43 +0200

    var,const,conversiones

--- IntroASwift.playground/Contents.swift
+++ IntroASwift.playground/Contents.swift
@@ -1,5 +1,25 @@
-//: Playground - noun: a place where people can play
+/*:
+ # Intro a Swift
+ 
+ */
+import Foundation
 
-import UIKit
+//: ## variables y constantes
+var size : Float = 42.0
+var answer = 42
 
-var str = "Hello, playground"
+let name = "Anakin"
+
+//: ## Todo es un objeto
+Int.max
+Double.abs(-42.0);
+
+//: ## conversiones
+
+let a = Int(size)
+let ans = String(answer)
+
+//: ## typealias: sirve para dar otros nombres a un tipo
+typealias Integer = Int
+
+let a1: Integer = 45
